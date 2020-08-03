2253e850cfc3489d97103988f79c96b7ca21a43f
zz <707308491@qq.com>
2020-08-03 18:38:46 +0800

    api-1.0[第一次]

--- apps/sdk_package/opencv/opencv.py
+++ apps/sdk_package/opencv/opencv.py
@@ -3,9 +3,9 @@
 # @Author : zhengzhong
 # @Software: PyCharm
 
-import subprocess
-res_p = subprocess.Popen("ldd /usr/local/ev_sdk/lib/libji.so |grep so.4.1",shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding="UTF-8")
-if res_p.stdout.readlines() == []:
+import os
+res_p = os.popen("ldd /usr/local/ev_sdk/lib/libji.so |grep so.4.1").read()
+if res_p is "":
     print(3.4)
 else:
     print(4.1)
